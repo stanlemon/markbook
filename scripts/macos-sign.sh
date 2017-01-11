a65bdbdda06379cbec08249bf04b38e77c4fafcc
Stan Lemon <stosh1985@gmail.com>
2017-01-11 08:27:32 -0500

    Add support for code signing on macOS

--- scripts/macos-sign.sh
+++ scripts/macos-sign.sh
@@ -1,11 +1,20 @@
+echo "Current operating system name:"
+echo $TRAVIS_OS_NAME
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    echo "Attempting to sign macOS binaries"
+
     export CERTIFICATE_P12=Certificate.p12;
     export KEYCHAIN=build.keychain;
 
-    echo $MACOS_SIGN_CERT | base64 — decode > $CERTIFICATE_P12;
+    echo $MACOS_SIGN_CERT | base64 --decode > $CERTIFICATE_P12;
 
+    echo "Creating keychain..."
     security create-keychain -p travisci $KEYCHAIN;
+    echo "Setting default keychain..."
     security default-keychain -s $KEYCHAIN;
+    echo "Unlocking keychain..."
     security unlock-keychain -p travisci $KEYCHAIN;
-    security import $CERTIFICATE_P12 -k $KEYCHAIN -T /usr/bin/codesign;
+    echo "Importing code signing certificate"
+    security import $CERTIFICATE_P12 -k $KEYCHAIN -T /usr/bin/codesign -P "";
 fi
