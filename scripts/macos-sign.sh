6569280bb4f9b95bca42af07500c5b326a89234c
Stan Lemon <stosh1985@gmail.com>
2017-03-11 14:03:48 -0500

    Update parameter for password

--- scripts/macos-sign.sh
+++ scripts/macos-sign.sh
@@ -16,7 +16,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     echo "Unlocking keychain..."
     security unlock-keychain -p travisci $KEYCHAIN;
     echo "Importing code signing certificate"
-    security import $CERTIFICATE_P12 -k $KEYCHAIN -P travisci -T /usr/bin/codesign;
+    security import $CERTIFICATE_P12 -k $KEYCHAIN -P $MACOS_SIGN_CERT_PW -T /usr/bin/codesign;
 
     security set-key-partition-list -S apple-tool:,apple: -s -k travisci $KEYCHAIN
 fi
